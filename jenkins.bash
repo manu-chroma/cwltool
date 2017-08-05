4e474fbddf1d01909970548bf4443dba924b0e50
Manvendra Singh <manvendra0310@gmail.com>
2017-08-06 00:02:06 +0530

    travis: try running comformance tests on travis

--- jenkins.bash
+++ jenkins.bash
@@ -1,10 +1,4 @@
 #!/bin/bash
-if [ "$JENKINS_URL" == ""]
-then
-	echo "Looks like we're not being run by Jenkins, this is dangerous"
-	echo "due to use of git clean -fdx command."
-	exit 1
-fi
 cloneorpull() {
         if test -d "$1" ; then
                 (cd "$1" && git pull)
@@ -14,39 +8,29 @@ cloneorpull() {
 }
 venv() {
         if ! test -d "$1" ; then
-                virtualenv -p python${PYTHON_VERSION} "$1"
+                virtualenv "$1"
         fi
 	# shellcheck source=/dev/null
         source "$1"/bin/activate
 }
-
 git clean --force -d -x || /bin/true
+pip install virtualenv
 cloneorpull common-workflow-language https://github.com/common-workflow-language/common-workflow-language.git
-docker pull node:slim
-
-# Test for Python 2.7 and Python 3
-for PYTHON_VERSION in 2.7 3
-do
-	venv cwltool-venv${PYTHON_VERSION}
-	export PIP_DOWNLOAD_CACHE=/var/lib/jenkins/pypi-cache/
-	# use pip2.7 and pip3 in separate loop runs
-	pip${PYTHON_VERSION} install -U setuptools wheel pip
-	pip${PYTHON_VERSION} install .
-	pip${PYTHON_VERSION} install "cwltest>=1.0.20160825151655"
-	pushd common-workflow-language
-	git clean --force -d -x || /bin/true
-	# shellcheck disable=SC2154
-	if [[ "$version" = *dev* ]]
-	then
-		EXTRA="EXTRA=--enable-dev"
-	fi
-	./run_test.sh --junit-xml=result.xml RUNNER=cwltool -j4 DRAFT=${version}
-	deactivate
-	CODE=$?
-	popd
-done
-
-# build new docker container
+venv cwltool-venv
+# docker pull node:slim
+pip install -U setuptools wheel pip
+python setup.py install
+pip install "cwltest>=1.0.20160825151655"
+pushd common-workflow-language
+git clean --force -d -x || /bin/true
+# shellcheck disable=SC2154
+if [[ "$version" = *dev* ]]
+then
+	EXTRA="EXTRA=--enable-dev"
+fi
+./run_test.sh --junit-xml=result.xml RUNNER=cwltool -j16 DRAFT="v1.0"
+CODE=$?
+popd
 if [ "$GIT_BRANCH" = "origin/master" ] && [[ "$version" = "v1.0" ]]
 then
   ./build-cwl-docker.sh && docker push commonworkflowlanguage/cwltool_module && docker push commonworkflowlanguage/cwltool
